eef6cc8f96f401b244dd143959e024e24b8d0162
Ollie Anwyll <oliver.anwyll@digital.justice.gov.uk>
2018-06-06 16:11:19 +0100

    Added policy and attached to user

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 resource "aws_sns_topic" "new_topic" {
-  name = "${var.topic_name}"
+  name         = "${var.topic_name}"
   display_name = "${var.display_name}" #No more than 10 chars
 }
 
@@ -11,5 +11,29 @@ resource "aws_iam_user" "new_topic_iam" {
 }
 
 resource "aws_iam_access_key" "new_topic_iam_access_key" {
-  user    = "${aws_iam_user.new_topic_iam.name}"
+  user = "${aws_iam_user.new_topic_iam.name}"
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    actions = [
+      "sns:*",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "${var.topic_name}-sns-topic-policy"
+  policy      = "${data.aws_iam_policy_document.policy.json}"
+  description = "Policy for SNS}"
+}
+
+resource "aws_iam_policy_attachment" "attach-policy" {
+  name       = "attached-policy"
+  users      = ["${aws_iam_user.new_topic_iam.name}"]
+  policy_arn = "${aws_iam_policy.policy.arn}"
 }
