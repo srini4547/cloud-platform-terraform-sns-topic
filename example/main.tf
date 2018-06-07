7d4386cc6156bbda1935b1659afa23813fd2ae95
Ollie Anwyll <oliver.anwyll@digital.justice.gov.uk>
2018-06-07 17:40:00 +0100

    example dir with example modules - Also removed k8s from root main.tf

---
+++ example/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "example_sns_topic" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sns?ref=master"
+
+  team_name          = "example-repo"
+  namespace          = "example-namespace"
+  topic_name         = "example-topic-name"
+  topic_display_name = "example-topic-display-name"
+}
+
+resource "kubernetes_secret" "example_k8_iam_secrets" {
+  metadata {
+    name      = "${aws_iam_user.new_topic_iam.name}-sns-user-secret"
+    namespace = "${var.cluster_namespace}"
+  }
+
+  data {
+    user_name         = "${aws_iam_user.new_topic_iam.name}"
+    access_key_id     = "${aws_iam_access_key.new_topic_iam_access_key.id}"
+    secret_access_key = "${aws_iam_access_key.new_topic_iam_access_key.secret}"
+    topic_arn         = "${aws_sns_topic.new_topic.arn}"
+  }
+}
---
+++ example/output.tf
@@ -0,0 +1,29 @@
+output "topic_arn" {
+  description = "ARN for the topic"
+  value       = "${module.example_topic_arn.arn}"
+}
+
+output "iam_name" {
+  description = "Name of IAM user that has been created"
+  value       = "${module.example_topic_iam.name}"
+}
+
+output "iam_arn" {
+  description = "ARN of IAM user that has been created"
+  value       = "${module.example_topic_iam.arn}"
+}
+
+# output "iam_unique_id" {
+#   description = "Unique ID of IAM user that has been created"
+#   value       = "${aws_iam_user.new_topic_iam.unique_id}"
+# }
+
+output "iam_access_key_id" {
+  description = "The access key ID"
+  value       = "${module.example_iam_access_key.id}"
+}
+
+output "iam_access_key_secret" {
+  description = "The secret access key ID"
+  value       = "${module.example_iam_access_key.secret}"
+}
