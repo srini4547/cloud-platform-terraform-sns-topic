0db15d89d59cdba334b7953c033f36efac7a92d3
Ollie Anwyll <oliver.anwyll@digital.justice.gov.uk>
2018-06-07 14:56:00 +0100

    Added README and amendment on outputs

--- output.tf
+++ output.tf
@@ -4,25 +4,26 @@ output "topic_arn" {
 }
 
 output "iam_name" {
-  description = "Name of IAM user that has been created"  
+  description = "Name of IAM user that has been created"
   value       = "${aws_iam_user.new_topic_iam.name}"
 }
 
 output "iam_arn" {
-  description = "ARN of IAM user that has been created"  
+  description = "ARN of IAM user that has been created"
   value       = "${aws_iam_user.new_topic_iam.arn}"
 }
 
 output "iam_unique_id" {
-  description = "Unique ID of IAM user that has been created"  
+  description = "Unique ID of IAM user that has been created"
   value       = "${aws_iam_user.new_topic_iam.unique_id}"
 }
 
 output "iam_access_key_id" {
-  description = "The access key ID"   
-  value = "${aws_iam_access_key.new_topic_iam_access_key.id}"
+  description = "The access key ID"
+  value       = "${aws_iam_access_key.new_topic_iam_access_key.id}"
 }
 
 output "iam_access_key_secret" {
-  value = "${aws_iam_access_key.new_topic_iam_access_key.secret}"
+  description = "The secret access key ID"
+  value       = "${aws_iam_access_key.new_topic_iam_access_key.secret}"
 }
