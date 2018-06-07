2bc57948bb7e10f6027338e0e42e6d6c50b620e7
Ollie Anwyll <oliver.anwyll@digital.justice.gov.uk>
2018-06-07 12:48:48 +0100

    Added K8s secrets and refined var file.

--- main.tf
+++ main.tf
@@ -34,3 +34,17 @@ resource "aws_iam_policy_attachment" "attach-policy" {
   users      = ["${aws_iam_user.new_topic_iam.name}"]
   policy_arn = "${aws_iam_policy.policy.arn}"
 }
+
+resource "kubernetes_secret" "k8_iam_secrets" {
+  metadata {
+    name      = "${aws_iam_user.new_topic_iam.name}-sns-user-secret"
+    namespace = "${var.cluster_namespace}"
+  }
+
+  data {
+    user_name         = "${aws_iam_user.new_topic_iam.name}"
+    access_key_id     = "${aws_iam_access_key.new_topic_iam_access_key.id}"
+    secret_access_key = "${aws_iam_access_key.new_topic_iam_access_key.secret}"
+    topic_arn         = "${aws_sns_topic.new_topic.arn}"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,5 +1,15 @@
-variable "topic_name" {}
+variable "topic_name" {
+  description = "The name of your SNS Topic"
+}
 
-variable "display_name" {}
+variable "display_name" {
+  description = "The display name of your SNS Topic. MUST BE UNDER 10 CHARS"
+}
 
-variable "team_name" {}
+variable "team_name" {
+  description = "The name of your team"
+}
+
+variable "cluster_namespace" {
+  description = "Your namespace within the cluster that you wish to deploy to."
+}
