b03acafa82f492b92fbae5734a91040a8b5da563
Mourad T <mourad.trabelsi@digital.justice.gov.uk>
2019-03-27 12:05:10 +0000

    bump module version in example

--- example/main.tf
+++ example/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
  *
  */
 module "example_sns_topic" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=1.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=2.0"
 
   team_name          = "example-team"
   topic_display_name = "example-topic-display-name"
