6d9870cbff21f8ba1e3ef1a197b00d82accba766
Alejandro Garrido Mota <garridomota@gmail.com>
2019-11-21 11:20:13 +0000

    Added variables type and upgrade to tf12

--- variables.tf
+++ variables.tf
@@ -1,12 +1,15 @@
 variable "topic_display_name" {
   description = "The display name of your SNS Topic. MUST BE UNDER 10 CHARS"
+  type        = string
 }
 
 variable "team_name" {
   description = "The name of your team"
+  type        = string
 }
 
 variable "aws_region" {
   description = "Region into which the resource will be created"
   default     = "eu-west-2"
+  type        = string
 }
